5b96454e06d15c35607ce39e32060a304e939938
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-12-17 03:18:21 -0500

    Add component exports

---
+++ src/components/index.tsx
@@ -0,0 +1,34 @@
+/*
+ *
+ * @section AboutSection 
+ * 
+ */
+export { AboutSection } from "./About.section";
+
+/*
+ *
+ * @section HeroSection 
+ * 
+ */
+export { HeroSection } from "./Hero.section";
+
+/*
+ *
+ * @section ExperienceSection 
+ * 
+ */
+export { ExperienceSection } from "./Experience.section";
+
+/*
+ *
+ * @section ProjectSection 
+ * 
+ */
+export { ProjectSection } from "./Projects.section";
+
+/*
+ *
+ * @component Footer 
+ * 
+ */
+export { Footer } from "./Footer";
