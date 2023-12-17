2c6cbf55628939737786e00afeba77dbe285c523
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-12-17 03:17:16 -0500

    Create exp section

---
+++ src/components/Experience.section.tsx
@@ -0,0 +1,15 @@
+const ExperienceSection = () => {
+    return (
+        <section id="experience-section">
+            <h1 className="relative flex text-3xl font-bold leading-none text-white">
+                Experience
+            </h1>
+            <br/>
+            <p className="text-white">
+                test
+            </p>
+        </section>
+    );
+};
+
+export { ExperienceSection };
