7de75cb3e96dd307f1d853cacda0e7d11d6a47f4
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-12-17 03:16:09 -0500

    Update landing page

--- src/pages/Landing.page.tsx
+++ src/pages/Landing.page.tsx
@@ -1,8 +1,33 @@
+import { AboutSection, ExperienceSection, Footer, HeroSection, ProjectSection } from "@components";
+
 const Landing = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <div className="relative z-20 h-auto overflow-x-hidden">
+            <div className="relative h-auto w-full">
+                <div className="relative mx-auto flex w-full max-w-4xl flex-col gap-16 overflow-hidden p-8 leading-relaxed max-md:mt-16 md:p-16">
+                    <div className="flex flex-col gap-12">
+                        <HeroSection />
+                        <p className="max-w-full overflow-hidden whitespace-nowrap text-accent-element">
+                            ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+                        </p>
+                        <AboutSection />
+                        <p className="max-w-full overflow-hidden whitespace-nowrap text-accent-element">
+                            ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+                        </p>
+                        <ExperienceSection />
+                        <p className="max-w-full overflow-hidden whitespace-nowrap text-accent-element">
+                            ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+                        </p>
+                        <ProjectSection />
+                        <p className="max-w-full overflow-hidden whitespace-nowrap text-accent-element">
+                            ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+                        </p>
+                        <div className="spacer" style={{ height: "2vh" }} />
+                        <Footer />
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
