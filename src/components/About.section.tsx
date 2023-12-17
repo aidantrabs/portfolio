9d996284468ba884ca0d9d9b6dfbfdcb0b70dbe8
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-12-17 03:17:01 -0500

    Create about section

---
+++ src/components/About.section.tsx
@@ -0,0 +1,15 @@
+const AboutSection = () => {
+    return (
+        <section id="about-section">
+            <h1 className="relative flex text-3xl font-bold leading-none text-white">
+                About
+            </h1>
+            <br/>
+            <p className="text-white">
+                test
+            </p>
+        </section>
+    );
+};
+
+export { AboutSection };
