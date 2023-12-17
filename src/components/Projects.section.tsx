377f6a1415fbc04091cb14d080432a5f73b527cc
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-12-17 03:17:59 -0500

    Create projects section

---
+++ src/components/Projects.section.tsx
@@ -0,0 +1,15 @@
+const ProjectSection = () => {
+    return (
+        <section id="projects-section">
+            <h1 className="relative flex text-3xl font-bold leading-none text-white">
+                Projects
+            </h1>
+            <br/>
+            <p className="text-white">
+                test
+            </p>
+        </section>
+    );
+};
+
+export { ProjectSection };
