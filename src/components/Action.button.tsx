3e18b6ef4aa09ca1392e5c99d494b1dfe422241b
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-03-07 04:57:24 -0500

    Add action button

---
+++ src/components/Action.button.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FadeSection } from '@components';
+
+interface ActionButtonProps {
+    description: string;
+    link: string;
+}
+
+const ActionButton: React.FC<ActionButtonProps> = ({ description, link }) => {
+    return (
+        <FadeSection>
+            <div className="flex justify-center mt-8">
+                <button 
+                    className="backdrop-blur-sm bg-white bg-opacity-5 rounded-lg overflow-hidden shadow-lg border border-gray-100 px-6 py-3 transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-105"
+                    onClick={() => window.open(link, '_blank noreferrer')}
+                >
+                    <span className="text-sm font-semibold text-white">{ description }</span>
+                </button>
+            </div>
+        </FadeSection>
+    );
+};
+
+export { ActionButton };
