2e6121238f85dd34085957651e9cc17ede1d230b
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-03-07 05:02:24 -0500

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,17 @@
-const Footer = () => {
+const Footer: React.FC = () => {
     return (
         <div className="flex justify-center items-center flex-col gap-4">
             <p className="text-sm opacity-70 text-white">
-            © 2024 Aidan Traboulay. All rights reserved.
+                © 2024 Aidan Traboulay. All rights reserved.
             </p>
+            
             <footer className="flex justify-center gap-8">
-
                 <a className="text-sm underline-offset-2 opacity-70 hover:underline text-white" 
-                    href="https://aidantraboulay.tech/">
+                    href="https://aidantraboulay.tech/Resume.pdf">
                     Résumé
                 </a>
                 <a className="text-sm underline-offset-2 opacity-70 hover:underline text-white" 
-                    href="">
+                    href="mailto:aidantraboulay@protonmail.com">
                     Email
                 </a>
             </footer>
