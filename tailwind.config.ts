fe9e80f297b046c12fe2e937655a2476fcbbaf4d
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-03-07 05:05:48 -0500

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,10 +10,32 @@ export default {
                 "accent-element": "#B8B3E9",
                 "primary-head": "#B8B3E9",
                 "primary-par": "#ECE4B7",
+                "nav-bg": "#2c1b3d",
+            },
+            
+            backdropBlur: {
+                xl: '40px',
+            },
+            
+            backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+
+            transitionDuration: {
+                '500': '500ms',
+            },
+
+            transitionTimingFunction: {
+                'in-expo': 'cubic-bezier(0.95, 0.05, 0.795, 0.035)',
+            },
+        }, 
+        
+        variants: {
+            extend: {
+                borderColor: ['hover', 'focus'],
+                transform: ['hover', 'focus'],
+                scale: ['hover', 'focus'],
             },
         },
     },
 
     plugins: [],
 }
-
