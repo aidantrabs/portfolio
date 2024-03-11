4ed32bb6e50223a4f5e62f09479f05a53640d59e
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-03-11 05:22:28 -0400

    Update action button

--- src/components/Action.button.tsx
+++ src/components/Action.button.tsx
@@ -12,7 +12,7 @@ const ActionButton: React.FC<ActionButtonProps> = ({ description, link }) => {
             <div className="flex justify-center mt-8">
                 <button 
                     className="backdrop-blur-sm bg-white bg-opacity-5 rounded-lg overflow-hidden shadow-lg border border-gray-100 px-6 py-3 transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-105"
-                    onClick={() => window.open(link, '_blank noreferrer')}
+                    onClick={() => window.open(link, '_blank noopener noreferrer')}
                 >
                     <span className="text-sm font-semibold text-white">{ description }</span>
                 </button>
