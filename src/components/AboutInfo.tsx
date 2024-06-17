5e932aef3cf179c889b2da6b3e0c62609134673e
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-06-17 19:05:38 -0400

    Update about info

--- src/components/AboutInfo.tsx
+++ src/components/AboutInfo.tsx
@@ -7,9 +7,10 @@ interface AboutInfoProps {
 
 const AboutInfo: React.FC<AboutInfoProps> = ({ Icon, title, description }) => {
     return (
-        <div className="flex flex-col items-center text-center">
+        <div className="flex flex-col items-center text-center border backdrop-blur-sm bg-white bg-opacity-5 border-white p-6 rounded-lg transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-105">
             <Icon size={35} className="mb-3"/>
             <h2 className="text-xl font-bold mb-2">{ title }</h2>
+            <hr className="w-1/2 border-accent-element border-2 mb-2"/>
             <p>
                 { description }
             </p>
