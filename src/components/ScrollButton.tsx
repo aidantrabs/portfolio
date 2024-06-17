47b9c562c6b21f25767d276119c59099118ec223
Aidan Traboulay <aidantraboulay13@gmail.com>
2024-06-17 19:06:52 -0400

    Add scroll button component

---
+++ src/components/ScrollButton.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-scroll';
+import { IoMdArrowUp } from "react-icons/io";
+
+/*
+ *
+ * @description Scroll button
+ * @props {void}
+ * 
+ */
+const ScrollButton: React.FC = () => {
+    return (
+        <div className="fixed bottom-6 right-8 z-50">
+            <Link
+                to="top"
+                smooth={true}
+                duration={500}
+                className="cursor-pointer"
+                offset={-75}
+            >
+                <div className="w-12 h-12 bg-accent-element rounded-full shadow-lg flex items-center justify-center">
+                    <IoMdArrowUp className="text-white" />
+                </div>
+            </Link>
+        </div>
+    );
+};
+
+export { ScrollButton };
