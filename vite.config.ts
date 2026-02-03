872470d72d9c5c8e535341bdccdb4bc6b8313d86
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-02-03 15:57:03 -0400

    feat: add tailwind to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    plugins: [tailwindcss()],
     build: {
         minify: 'terser',
         cssMinify: true,
